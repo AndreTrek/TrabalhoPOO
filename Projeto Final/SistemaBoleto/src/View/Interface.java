27fe91ff01986d8527861ac5afff728dd8faf44d
Marcos Souza <marcos13@outlook.com>
2016-12-07 18:11:05 -0200

    New Commit - Interface
    Rename class "tela.java" para "interface.java"

--- Projeto Final/SistemaBoleto/src/View/Interface.java
+++ Projeto Final/SistemaBoleto/src/View/Interface.java
@@ -9,12 +9,12 @@ package View;
  *
  * @author Marcos
  */
-public class Tela extends javax.swing.JFrame {
+public class Interface extends javax.swing.JFrame {
 
     /**
      * Creates new form Tela
      */
-    public Tela() {
+    public Interface() {
         initComponents();
     }
 
@@ -697,7 +697,7 @@ public class Tela extends javax.swing.JFrame {
 
         localDePagamentoDoBoletojLabel.setText("Local de Pagamento");
 
-        localDePagamentoDoBoletojTextField.setToolTipText("Digite");
+        localDePagamentoDoBoletojTextField.setToolTipText("Digite o Local de Pagamento");
 
         instrucaoAoSacadoDoBoletojLabel.setText("Instrução ao Sacado");
 
@@ -907,20 +907,21 @@ public class Tela extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Tela().setVisible(true);
+                new Interface().setVisible(true);
             }
         });
     }
